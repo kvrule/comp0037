30b6cfb0a661b2d8312f227835598af62e0ab292
Granit <granitdula98@gmail.com>
2020-03-13 23:00:14 +0000

    Removed unecessary default code.

--- comp0037_explorer/src/comp0037_explorer/explorer_node.py
+++ comp0037_explorer/src/comp0037_explorer/explorer_node.py
@@ -78,38 +78,6 @@ class ExplorerNode(ExplorerNodeBase):
         
         return candidateGood, destination
 
-#    def chooseNewDestination(self):
-
-
-# #         print 'blackList:'
-# #         for coords in self.blackList:
-# #             print str(coords)
-
-#         candidateGood = False
-#         destination = None
-#         smallestD2 = float('inf')
-
-#         for x in range(0, self.occupancyGrid.getWidthInCells()):
-#             for y in range(0, self.occupancyGrid.getHeightInCells()):
-#                 candidate = (x, y)
-#                 if self.isFrontierCell(x, y) is True:
-#                     candidateGood = True
-#                     for k in range(0, len(self.blackList)):
-#                         if self.blackList[k] == candidate:
-#                             candidateGood = False
-#                             break
-                    
-#                     if candidateGood is True:
-#                         d2 = candidate[0]**2+(candidate[1]-0.5*self.occupancyGrid.getHeightInCells())**2
-
-#                         if (d2 < smallestD2):
-#                             destination = candidate
-#                             smallestD2 = d2
-
-#         # If we got a good candidate, use it
-
-#         return candidateGood, destination
-
     def destinationReached(self, goal, goalReached):
         if goalReached is False:
 #             print 'Adding ' + str(goal) + ' to the naughty step'
